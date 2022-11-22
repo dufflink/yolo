eedd3c91fd866a213a533a91011e754b3199a7bd
dufflink <skoryninmaksim1@gmail.com>
2022-11-22 14:17:37 +0400

    Added Logo and removed Event List view

--- Yolo/Modules/MainView.swift
+++ Yolo/Modules/MainView.swift
@@ -15,74 +15,37 @@ final class GameListModel: ObservableObject {
     
 }
 
-class EventManager: ObservableObject {
-    
-    @Published var events: [EventListModel] = [
-        .init(name: "ESL Pro league", tier: 1, selected: false),
-        .init(name: "Blast", tier: 1, selected: true),
-        .init(name: "DCL Pro league", tier: 2, selected: false),
-        .init(name: "First arrow season XII", tier: 3, selected: true),
-        .init(name: "IEM Rio", tier: 1, selected: true)
-    ]
-    
-}
-
 struct MainView: View {
     
     @StateObject private var matchesModel = MatchesModel()
     @StateObject private var gameListModel = GameListModel()
     
-    @StateObject private var eventManager = EventManager()
-    
-    @State private var showSheet = false
-    
     var body: some View {
-            NavigationView {
-                ZStack(alignment: .top) {
-                    MatchList(matches: matchesModel.matches, bottomInset: 48, isLoading: $matchesModel.isLoading)
-                        .headerView {
-                            GameList(games: gameListModel.games, selectedGame: $matchesModel.currentGame)
-                        }.padding(.top, 42)
-                    VStack(alignment: .leading) {
-                        Text("Yolo")
-                            .font(.system(size: 30, weight: .bold))
-                            .padding(.horizontal, 16)
-                        Spacer()
-                        MatchStatusList(mathStatuses: matchesModel.availableMatchStatuses, selectedStatus: $matchesModel.selectedMatchStatus, events: $eventManager.events, tapOnTrophy: {
-                                setSheetState(isHidden: true)
-                            })
-                        .shadow(color: Color.black.opacity(0.15), radius: 12, x: 4, y: 4)
-                        .offset(y: showSheet ? -40 : .zero)
-                    }
+        NavigationView {
+            ZStack(alignment: .top) {
+                MatchList(matches: matchesModel.matches, bottomInset: 48, isLoading: $matchesModel.isLoading)
+                    .headerView {
+                        GameList(games: gameListModel.games, selectedGame: $matchesModel.currentGame)
+                    }.padding(.top, 42)
+                VStack(alignment: .leading) {
+                    HStack(spacing: 8) {
+                        Image("LogoTransparent")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.black)
+                        
+                    }.padding(.leading, 16)
                     
-                    GeometryReader { geometry in
-                        VStack {
-                            Spacer()
-                            EventList(events: eventManager.events, didPressCloseButton: {
-                                withAnimation {
-                                    setSheetState(isHidden: false)
-                                }
-                            }, didPressSaveButton: { events in
-                                eventManager.events = events
-                                
-                                withAnimation {
-                                    setSheetState(isHidden: false)
-                                }
-                            })
-                            .offset(y: showSheet ? -24 : geometry.size.height)
-                        }
-                    }
+                    Spacer()
+                    MatchStatusList(mathStatuses: matchesModel.availableMatchStatuses, selectedStatus: $matchesModel.selectedMatchStatus)
+                    .shadow(color: Color.black.opacity(0.15), radius: 12, x: 4, y: 4)
+                }
             }
         }.onAppear {
             matchesModel.getMatches()
         }
     }
     
-    func setSheetState(isHidden: Bool) {
-        withAnimation(isHidden ? .easeOut : .easeInOut) {
-            showSheet = isHidden
-        }
-    }
 }
 
 struct MainView_Previews: PreviewProvider {
--- Yolo/Views/MatchStatusList/MatchStatusList.swift
+++ Yolo/Views/MatchStatusList/MatchStatusList.swift
@@ -12,9 +12,6 @@ struct MatchStatusList: View {
     let mathStatuses: [Match.Status]
     
     @Binding var selectedStatus: Match.Status
-    @Binding var events: [EventListModel]
-    
-    var tapOnTrophy: (() -> Void) = { }
     
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
@@ -31,32 +28,6 @@ struct MatchStatusList: View {
                             selectedStatus = mathStatus
                         }
                 }
-                
-                ZStack(alignment: .bottomTrailing) {
-                    Image(systemName: "trophy.fill")
-                        .opacity(0.8)
-                        .frame(width: 41, height: 44)
-                        .background(Color.yellow)
-                        .foregroundColor(Color.black.opacity(0.8))
-                        .clipShape(Circle())
-                    
-                    let selectedCount = events.filter { $0.selected == true }.count
-                    
-                    if selectedCount > 0 {
-                        Text("\(selectedCount)")
-                            .frame(width: 20, height: 20)
-                            .font(.system(size: 14, weight: .bold))
-                            .foregroundColor(Color.white)
-                            .background(Color.red)
-                            .clipShape(Circle())
-                            .offset(x: 4)
-                    }
-                }
-                .padding(.horizontal, 8)
-                .onTapGesture {
-                    tapOnTrophy()
-                }
-                
             }
             .padding(.horizontal, 16)
         }
@@ -67,10 +38,6 @@ struct MatchStatusList_Previews: PreviewProvider {
     static var previews: some View {
         MatchStatusList(mathStatuses: [
             .running, .notStarted, .finished
-        ], selectedStatus: .constant(.running), events: .constant([
-            .init(name: "", tier: 1, selected: true),
-            .init(name: "", tier: 1, selected: false),
-            .init(name: "", tier: 1, selected: false)
-        ]))
+        ], selectedStatus: .constant(.running))
     }
 }
